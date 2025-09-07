202b62993cb62f79a9c8b68f6e1cdcedcbcae5a6
Khadka <120006828+khadka27@users.noreply.github.com>
2025-09-07 13:53:50 +0545

    Remove old favicon references and add new SVG favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,16 +37,6 @@ export const metadata: Metadata = {
   authors: [{ name: "ReviewCraft Team" }],
   creator: "ReviewCraft",
   publisher: "ReviewCraft",
-  icons: {
-    icon: [
-      { url: "/favicon.ico" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-    ],
-    apple: [
-      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
-    ],
-  },
   manifest: "/site.webmanifest",
   robots: "index, follow",
   openGraph: {
